4a60b0840669a4433dc68ca3587ef66cdaa27bb4
Lado <lado.asambadze1@gmail.com>
2025-01-18 13:05:46 +0400

    remove tepmorary transfers handlers

--- src/routes/api/transfers.ts
+++ src/routes/api/transfers.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { createTransfer, deleteTransfer, getAllTransfers, updateTransfer } from "../../handlers/transfers";
+import {  getAllTransfers  } from "../../handlers/transfers";
  
 const transfersRouter = Router();
  
 
-transfersRouter.post("/create_transfers",  createTransfer);
+// transfersRouter.post("/create_transfers",  createTransfer);
 transfersRouter.get("/all_transfers",  getAllTransfers);
-transfersRouter.put("/update_transfer",  updateTransfer);
-transfersRouter.delete("/transfer/:id",  deleteTransfer);
+// transfersRouter.put("/update_transfer",  updateTransfer);
+// transfersRouter.delete("/transfer/:id",  deleteTransfer);
 
 
 
