4a1813d8c5f33184c1cdb28551e3b9276de33229
Lado <lado.asambadze1@gmail.com>
2025-07-03 15:30:59 +0400

    udate urls

--- src/handlers/payments/handleBOGPayment.ts
+++ src/handlers/payments/handleBOGPayment.ts
@@ -175,8 +175,8 @@ export const handleBOGPayment = async (
           ],
         },
         redirect_urls: {
-          success: `http://localhost:3000/payment/success?order_id=${external_order_id}`,
-          fail: `http://localhost:3000/payment/failure?order_id=${external_order_id}`,
+          success: `${process.env.FRONTEND_CALLBACK}/payment/success?order_id=${external_order_id}`,
+          fail: `${process.env.FRONTEND_CALLBACK}/payment/failure?order_id=${external_order_id}`,
         },
         ttl: 30,
       };
