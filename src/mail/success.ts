8d624ebb77a29479c8d6db15f123a4cba8ca9302
Lado <lado.asambadze1@gmail.com>
2025-07-07 17:35:58 +0400

    fix email sending

--- src/mail/success.ts
+++ src/mail/success.ts
@@ -20,7 +20,7 @@ export const sendPaymentSuccessEmail = async (
     `;
     const { data, error } = await transporter.emails.send({
       from: process.env.RESEND_FROM_EMAIL || "noreply@daudtravel.com",
-      to: email && "traveldaud@gmail.com",
+      to: [email, "traveldaud@gmail.com"],
       subject,
       text,
       html,
