87c0ba1b574746225b8d5c6b38917c3428fd1160
Lado <lado.asambadze1@gmail.com>
2025-07-07 12:53:40 +0400

    plus one

--- src/routes/api/payment.ts
+++ src/routes/api/payment.ts
@@ -4,13 +4,14 @@ import { getPaymentOrder } from "../../handlers/payments/getPaymentOrders";
 import { handleBOGPayment } from "../../handlers/payments/handleBOGPayment";
 import { handleBOGCallback } from "../../handlers/payments/handleBogCallback";
 import { getPaymentOrderById } from "../../handlers/payments/getPaymentByOrder";
+import verifyToken from "../../middlewares/auth-middleware";
 
 const bogPaymentsRouter = Router();
 
 bogPaymentsRouter.post("/payments/bog/create", handleBOGPayment);
 bogPaymentsRouter.post("/payments/bog/callback", handleBOGCallback);
 bogPaymentsRouter.get("/payments/bog/status/:order_id", getBOGPaymentStatus);
-bogPaymentsRouter.get("/orders", getPaymentOrder);
+bogPaymentsRouter.get("/orders", verifyToken, getPaymentOrder);
 bogPaymentsRouter.get("/orders/:id", getPaymentOrderById);
 
 export default bogPaymentsRouter;
